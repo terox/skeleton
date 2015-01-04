937fa4938f9d385edad988f407b8b77bf862cae7
David Pérez Terol <davidtxnet@gmail.com>
2015-01-04 18:46:33 +0100

    Add before function in manifest. Test have more accuracy

--- test/fixtures/manifest.js
+++ test/fixtures/manifest.js
@@ -4,10 +4,29 @@ module.exports = {
     name: 'unnamed'
   },
 
+  before: function(scope, data) {
+
+    var self = this;
+
+    if(typeof data.components !== "undefined") {
+
+      data.components.forEach(function(component) {
+
+        self.scaffold['app/components/' + component + '.txt'] = {
+          action : 'template',
+          path   : 'templates/component.tpl',
+          data   : { component: component}
+        };
+
+      });
+
+    }
+  },
+
   scaffold: {
     'app/components' : { action: 'folder',   mode: 0777 },
     'app/README.md'  : { action: 'template', path: 'templates/README.tpl', data: { name: 'hello world!'} },
-    'app/LICENSE.md' : { action: 'template', path: 'templates/LICENSE.tpl', data: { name: 'license' } },
+    'app/LICENSE.md' : { action: 'template', path: 'templates/LICENSE.tpl'},
     'app/assets/'    : { action: 'copy',     path: 'assets/subdir' },
     'app/<%= name %>': { action: 'folder',   mode: 0777 }
   }
